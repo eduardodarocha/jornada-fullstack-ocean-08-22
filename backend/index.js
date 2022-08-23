384daa48c5574998a39e2f659cd883919502bdb8
Eduardo da Rocha <eduardomfrocha@gmail.com>
2022-08-22 23:03:15 -0300

    create link w/ MongoDB & endpoints

--- backend/index.js
+++ backend/index.js
@@ -1,51 +1,81 @@
 const express = require('express');
-const app = express();
-app.use(express.json());
+const { MongoClient } = require('mongodb');
 
-app.get('/', function (req, res) {
-  console.log("res",res);
-  res.send(`<h1>'Hello World!!!'</h1>`);
-});
+// const url = "mongodb+srv://admin:beeDsNaD7hrUJflY@cluster0.0tjjv1e.mongodb.net/";
+const url = "mongodb://localhost:27017";
+const dbName = "jornada-fullstack-ocean-08-22";
 
-app.get('/oi', function (req, res) {
-  console.log("res",res);
-  res.send(`<h1>'Alô mundo!'</h1>`);
-});
-console.log("Servidor rodando na porta 3000");
+// Declaração da função main()
+async function main() {
+  // Realizar a conexão com o MongoClient
+  // MongoClient -> MongoDatabase -> MongoCollection
 
-const lista = [
-  {
-    id: 1,
-    nome: "Paulo",
-    pontos: 21,
-  },
-  {
-    id: 2,
-    nome: "Daniel",
-    pontos: 52,
-  },
-  {
-    id: 3,
-    nome: "Beatriz",
-    pontos: 97,
-  },
-];
+  // Conexões com o client podem levar um tempo para
+  //  concluir. Portanto, utilizamos o mecanismo de
+  //  Promises do JavaScript, que permitem aguardar
+  //  esse tempo. Para isso, vamos usar o async/await.
 
-app.get("/pontuacoes", function (req, res) {
-  res.send(lista);
-});
+  console.log("Conectando com o banco de dados...");
 
-app.post("/pontuacoes", function (req, res) {
-  const item = req.body;
-  // console.log(item);
-  lista.push({
-    id: lista.length + 1,
-    nome: item.nome,
-    pontos: item.pontos,
+  const client = await MongoClient.connect(url);
+  const db = client.db(dbName);
+  const collection = db.collection("pontuacoes");
+
+  console.log("Banco de dados conectado com sucesso!");
+
+
+  const app = express();
+  app.use(express.json());
+
+  app.get('/', function (req, res) {
+    console.log("res", res);
+    res.send(`<h1>'Hello World!!!'</h1>`);
   });
-  res.send("Item adicionado com sucesso!");
 
-  res.send(lista);
-})
+  app.get('/oi', function (req, res) {
+    console.log("res", res);
+    res.send(`<h1>'Alô mundo!'</h1>`);
+  });
+  console.log("Servidor rodando na porta 3000");
 
-app.listen(3000);
+  // const lista = [{
+  //     id: 1,
+  //     nome: "Paulo",
+  //     pontos: 21,
+  //   },
+  //   {
+  //     id: 2,
+  //     nome: "Daniel",
+  //     pontos: 52,
+  //   },
+  //   {
+  //     id: 3,
+  //     nome: "Beatriz",
+  //     pontos: 97,
+  //   },
+  // ];
+
+  app.get("/pontuacoes", async function (req, res) {
+    const itens = await collection
+    .find()
+    .sort({ pontos: -1 })
+    .limit(10)
+    .toArray();
+    res.send(itens);
+  });
+
+  app.post("/pontuacoes", async function (req, res) {
+    const item = req.body;
+    // console.log(item);
+    // lista.push({
+    //   id: lista.length + 1,
+    //   nome: item.nome,
+    //   pontos: item.pontos,
+    // });
+    await collection.insertOne(item)
+    res.send(item);
+  });
+
+  app.listen(3000);
+}
+main();
