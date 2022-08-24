38ea38da25f013f120517c5b425eb9faa1ae263e
Eduardo da Rocha <eduardomfrocha@gmail.com>
2022-08-24 16:33:13 -0300

    Add & stilyzed imgs

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,11 +1,15 @@
 import React from "react";
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
+import mario from "../../assets/mario.gif";
+import cano from "../../assets/pipe.png";
 
 function Jogo() {
   return (
     <div className="jogo">
-      <img src={clouds} alt="nuvens" />
+      <img className="nuvens" src={clouds} alt="nuvens" />
+      <img className="mario" src={mario} alt="mario" />
+      <img className="cano" src={cano} alt="cano" />
     </div>
   );
 }
