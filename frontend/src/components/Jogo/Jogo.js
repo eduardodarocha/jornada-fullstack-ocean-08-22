da1bd4bd99261ea1abc8601f9c4f00be8a7c6fd5
Eduardo da Rocha <eduardomfrocha@gmail.com>
2022-08-24 18:03:32 -0300

    Create Mario's jump animation

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,10 +1,16 @@
-import React from "react";
+import React, {useState} from "react";
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
 import cano from "../../assets/pipe.png";
 
 function Jogo() {
+  const [estaPulando, setEstaPulando] = useState(false);
+  document.onkeydown = function () {
+    
+    console.log("OnKeyDown");    
+  }
+
   return (
     <div className="jogo">
       <img className="nuvens" src={clouds} alt="nuvens" />
