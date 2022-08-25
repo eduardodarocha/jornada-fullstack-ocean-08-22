a70f190b386d415c548e78af5db80b83cd8dfbe2
Eduardo Rocha <eduardomfrocha@gmail.com>
2022-08-25 17:30:20 -0300

    Add Game Over logic (WIP)

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,21 +1,56 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
 import cano from "../../assets/pipe.png";
 
+
 function Jogo() {
   const [estaPulando, setEstaPulando] = useState(false);
-  document.onkeydown = function () {
+  const marioRef = useRef();
+  const canoRef = useRef();  
+
+  function marioEstaNoCano() {
+    const mario = marioRef.current;
+    const cano = canoRef.current;
     
-    console.log("OnKeyDown");    
+    if (!mario || !cano) {
+      return;
+    };
+  
+    return (
+      cano.offsetLeft > mario.offsetLeft &&
+      cano.offsetLeft < mario.offsetLeft + mario.offsetWidth &&
+      mario.offsetTop + mario.offsetHeight > cano.offsetTop
+    );
   }
+
+  setInterval(() => {
+    const valor = marioEstaNoCano();
+    console.log("Mario está no cano", valor);
+  }, 100);
+
+  document.onkeydown = function () {    
+    console.log("OnKeyDown");    
+    setEstaPulando(true);
+    setTimeout(() => {
+      setEstaPulando(false);
+    }, 700);
+  }
+
+  let marioClassName = "mario";
+
+  if (estaPulando) {
+    marioClassName = "mario mario-pulo";
+  }
+
+  console.log(estaPulando);
 
   return (
     <div className="jogo">
       <img className="nuvens" src={clouds} alt="nuvens" />
-      <img className="mario" src={mario} alt="mario" />
-      <img className="cano" src={cano} alt="cano" />
+      <img  ref={marioRef} className={marioClassName} src={mario} alt="mario" />
+      <img ref={canoRef} className="cano" src={cano} alt="cano" />
       <div className="chao"></div>
     </div>
   );
