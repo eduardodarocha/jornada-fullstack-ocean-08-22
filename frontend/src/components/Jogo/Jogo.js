6e3bad70b2623c1711b174941746f552c0b7ace0
Eduardo Rocha <eduardomfrocha@gmail.com>
2022-08-31 17:46:28 -0300

    Created game over

--- frontend/src/components/Jogo/Jogo.js
+++ frontend/src/components/Jogo/Jogo.js
@@ -1,23 +1,25 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import "./Jogo.css";
 import clouds from "../../assets/clouds.png";
 import mario from "../../assets/mario.gif";
+import gameOver from "../../assets/game-over.png";
 import cano from "../../assets/pipe.png";
-
 
 function Jogo() {
   const [estaPulando, setEstaPulando] = useState(false);
+  const [estaMorto, setEstaMorto] = useState(false);
+
   const marioRef = useRef();
-  const canoRef = useRef();  
+  const canoRef = useRef();
 
   function marioEstaNoCano() {
     const mario = marioRef.current;
     const cano = canoRef.current;
-    
+
     if (!mario || !cano) {
       return;
-    };
-  
+    }
+
     return (
       cano.offsetLeft > mario.offsetLeft &&
       cano.offsetLeft < mario.offsetLeft + mario.offsetWidth &&
@@ -26,17 +28,24 @@ function Jogo() {
   }
 
   setInterval(() => {
-    const valor = marioEstaNoCano();
-    console.log("Mario está no cano", valor);
+    const estaNoCano = marioEstaNoCano();
+    // console.log("Mario está no cano", valor);
+
+    if (!estaNoCano) {
+      return;
+    }
+    setEstaMorto(true);
   }, 100);
 
-  document.onkeydown = function () {    
-    console.log("OnKeyDown");    
+  console.log({ estaMorto });
+
+  document.onkeydown = function () {
+    console.log("OnKeyDown");
     setEstaPulando(true);
     setTimeout(() => {
       setEstaPulando(false);
     }, 700);
-  }
+  };
 
   let marioClassName = "mario";
 
@@ -44,13 +53,17 @@ function Jogo() {
     marioClassName = "mario mario-pulo";
   }
 
-  console.log(estaPulando);
+  // console.log(estaPulando);
+
+  const marioImage = estaMorto ? gameOver : mario;
+
+  const pararAnimacao = estaMorto ? "parar-animacao" : "";
 
   return (
     <div className="jogo">
       <img className="nuvens" src={clouds} alt="nuvens" />
-      <img  ref={marioRef} className={marioClassName} src={mario} alt="mario" />
-      <img ref={canoRef} className="cano" src={cano} alt="cano" />
+      <img ref={marioRef} className={marioClassName} src={marioImage} alt="mario" />
+      <img ref={canoRef} className={"cano " + pararAnimacao } src={cano} alt="cano" />
       <div className="chao"></div>
     </div>
   );
